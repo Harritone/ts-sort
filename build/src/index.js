dc94f109616ae2d64cd7cbc47d842cf7e856c061
Harritone <yourrasik@gmail.com>
2020-12-15 16:27:14 +1000

    Refactored code for sorting numbers

---
+++ build/src/index.js
@@ -0,0 +1,32 @@
+"use strict";
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                // if collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+                // if collection is a string
+                if (typeof this.collection === 'string') {
+                    this.collection.split('').sort().join('');
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
+var string = new Sorter('asdfasklllj');
+string.sort();
+console.log(string.collection);
